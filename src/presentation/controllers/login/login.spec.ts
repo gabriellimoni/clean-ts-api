14f161bd728afb5e74a2e999e9f88b54374e9348
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-12-23 15:13:05 -0300

    refactor: move creations to factory helper methods

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,29 +1,39 @@
 import { LoginController } from './login'
 import { badRequest } from '../../helpers/http-helper'
 import { MissingParamError } from '../../errors'
+import { HttpRequest } from '../../protocols'
+
+interface SutTypes {
+  sut: LoginController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LoginController()
+
+  return {
+    sut
+  }
+}
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    email: 'any_email@mail.com',
+    password: 'any_passwd'
+  }
+})
 
 describe('Login Controller', () => {
   test('Should return 400 if no email is provided', async () => {
-    const sut = new LoginController()
-    const httpRequest = {
-      body: {
-        email: 'any_email@mail.com',
-        password: 'any_passwd'
-      }
-    }
+    const { sut } = makeSut()
+    const httpRequest = makeFakeRequest()
     delete httpRequest.body.email
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
 
   test('Should return 400 if no password is provided', async () => {
-    const sut = new LoginController()
-    const httpRequest = {
-      body: {
-        email: 'any_email@mail.com',
-        password: 'any_passwd'
-      }
-    }
+    const { sut } = makeSut()
+    const httpRequest = makeFakeRequest()
     delete httpRequest.body.password
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
