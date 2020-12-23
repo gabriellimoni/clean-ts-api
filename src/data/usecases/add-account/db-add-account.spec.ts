279d406b107da5321b4c0508e4e8835ab69be4a7
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-12-22 22:27:34 -0300

    refactor: group all protocols in a single file

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,4 +1,4 @@
-import { Encrypter } from '../../protocols/Encrypter'
+import { Encrypter } from './db-add-account-protocols'
 import { DbAddAccount } from './db-add-account'
 
 const makeEncrypter = (): Encrypter => {
