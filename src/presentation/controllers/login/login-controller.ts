67d8f9eac5e4e10207d02ccb4cf3fbcb21119417
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-12-24 14:26:01 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -3,13 +3,10 @@ import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/ht
 import { Validation } from '../signup/signup-controller-protocols'
 
 export class LoginController implements Controller {
-  private readonly authentication: Authentication
-  private readonly validation: Validation
-
-  constructor (authentication: Authentication, validation: Validation) {
-    this.authentication = authentication
-    this.validation = validation
-  }
+  constructor (
+    private readonly authentication: Authentication,
+    private readonly validation: Validation
+  ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
