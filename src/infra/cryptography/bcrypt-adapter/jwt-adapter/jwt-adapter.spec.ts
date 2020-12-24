85869669830b82646e698be34b28074c7ea42b3c
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-12-24 13:06:02 -0300

    test: ensure JwtAdapter returns access token on success

--- src/infra/cryptography/bcrypt-adapter/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter/jwt-adapter/jwt-adapter.spec.ts
@@ -1,5 +1,11 @@
-import { JwtAdapter } from './jwt-adapter'
 import jwt from 'jsonwebtoken'
+import { JwtAdapter } from './jwt-adapter'
+
+jest.mock('jsonwebtoken', () => ({
+  async sign (): Promise<string> {
+    return 'any_token'
+  }
+}))
 
 describe('Jwt Adapter', () => {
   test('Should call sign with correct values', async () => {
@@ -7,5 +13,11 @@ describe('Jwt Adapter', () => {
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+
+  test('Should return a token on sign success', async () => {
+    const sut = new JwtAdapter('secret')
+    const accessToken = await sut.encrypt('any_id')
+    expect(accessToken).toBe('any_token')
   })
 })
