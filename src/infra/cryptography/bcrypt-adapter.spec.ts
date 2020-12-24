21ab5a576dec1ca1d8995f0388716776fbca5ec9
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-12-24 12:42:30 -0300

    test: ensure BcryptAdapter returns false when bcrypt compare fails

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -49,10 +49,17 @@ describe('Bcrypt Adapter', () => {
     expect(isValid).toBe(true)
   })
 
-  test('Should throw if bcrypt hash throws', async () => {
+  test('Should throw if bcrypt compare throws', async () => {
     const sut = makeSut()
     jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const promise = sut.compare('any_value', 'any_hash')
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return false if bcrypt compare fails', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(new Promise(resolve => resolve(false)))
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBe(false)
   })
 })
