5ee5766c46c153a68c5efdec17e7ba41c226328f
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-12-30 00:50:04 -0300

    feat: ensure add survey route returns 403 if no x-access-token is provided

---
+++ src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,19 @@
+import { HttpRequest, Middleware } from '../../presentation/protocols'
+import { NextFunction, Request, Response } from 'express'
+
+export const adaptMiddleware = (middleware: Middleware) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const httpRequest: HttpRequest = {
+      headers: req.headers
+    }
+    const httpResponse = await middleware.handle(httpRequest)
+    if (httpResponse.statusCode === 200) {
+      Object.assign(req, httpResponse.body)
+      return next()
+    }
+
+    return res.status(httpResponse.statusCode).json({
+      error: httpResponse.body.message
+    })
+  }
+}
---
+++ src/main/factories/usecases/account/load-account-by-token/db-load-account-by-token-factory.ts
@@ -0,0 +1,11 @@
+import { DbLoadAccountByToken } from '../../../../../data/usecases/load-account-by-token/db-load-account-by-token'
+import { LoadAccountByToken } from '../../../../../domain/usecases/load-account-by-token'
+import { JwtAdapter } from '../../../../../infra/cryptography/jwt-adapter/jwt-adapter'
+import { AccountMongoRepository } from '../../../../../infra/db/mongodb/account/account-mongo-repository'
+import env from '../../../../config/env'
+
+export const makeDbLoadAccountByToken = (): LoadAccountByToken => {
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const accountMongoRepository = new AccountMongoRepository()
+  return new DbLoadAccountByToken(jwtAdapter, accountMongoRepository)
+}
