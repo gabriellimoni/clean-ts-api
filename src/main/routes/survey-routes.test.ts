48ad4bed137b7bcde1e397daa8d7c3af572888ad
Gabriel Limoni <gabrielblimoni@gmail.com>
2020-12-30 00:56:45 -0300

    test: ensure add survey route returns 204 if valid x-access-token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,8 +2,21 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
+
+const makeFakeRequest = (): any => ({
+  question: 'Question',
+  answers: [{
+    answer: 'Answer 1',
+    image: 'http://image-name.com'
+  },{
+    answer: 'Answer 1'
+  }]
+})
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -16,23 +29,41 @@ describe('Survey Routes', () => {
 
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await surveyCollection.deleteMany({})
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /surveys', () => {
     test('Should return 403 on add survey without x-access-token', async () => {
       await request(app)
         .post('/api/surveys')
-        .send({
-          question: 'Question',
-          answers: [{
-            answer: 'Answer 1',
-            image: 'http://image-name.com'
-          },{
-            answer: 'Answer 1'
-          }]
-        })
+        .send(makeFakeRequest())
         .expect(403)
+    })
+
+    test('Should return 204 on add survey with valid x-access-token', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email',
+        password: 123,
+        role: 'admin'
+      })
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send(makeFakeRequest())
+        .expect(204)
     })
   })
 })
