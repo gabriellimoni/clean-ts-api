6768d50df023b83d8818475bd582559e996a7df5
Gabriel Limoni <gabrielblimoni@gmail.com>
2021-01-02 17:31:31 -0300

    feat: ensure LoadSurveysController calls LoadSurvey

---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -0,0 +1,10 @@
+import { HttpRequest, HttpResponse, Controller, LoadSurveys } from './load-survey-controller-protocols'
+
+export class LoadSurveysController implements Controller {
+  constructor (private readonly loadSurveys: LoadSurveys) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.loadSurveys.load()
+    return null
+  }
+}
