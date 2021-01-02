253d8ece0c686040b9a3e52e74f2389bf0738af9
Gabriel Limoni <gabrielblimoni@gmail.com>
2021-01-02 17:53:06 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -0,0 +1,11 @@
+import { SurveyModel } from '../../../domain/models/survey'
+import { LoadSurveys } from '../../../domain/usecases/load-surveys'
+import { LoadSurveysRepository } from '../../protocols/db/survey/load-surveys-repository'
+
+export class DbLoadSurveys implements LoadSurveys {
+  constructor (private readonly loadSurveysRepository: LoadSurveysRepository) {}
+  async load (): Promise<SurveyModel[]> {
+    this.loadSurveysRepository.loadAll()
+    return null
+  }
+}
